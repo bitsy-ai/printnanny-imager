43240570db503ce7487047b4a49b94f2b89168f9
Leigh Johnson <leigh@bitsy.ai>
2022-09-09 16:11:59 -0700

    clean up unused rust code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,16 +3,8 @@
     windows_subsystem = "windows"
 )]
 
-use printnanny_imager::disks::get_serializable_disks;
-
-#[tauri::command]
-fn list_disks() -> String {
-    get_serializable_disks()
-}
-
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![list_disks])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
